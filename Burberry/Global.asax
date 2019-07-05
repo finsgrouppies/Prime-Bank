d3663d862923e50193c719a33c6d37b575903c46
finsgrouppies <finsgrouppies@gmail.com>
2019-07-05 14:27:46 +0300

    Add project files.

---
+++ Burberry/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Burberry.MvcApplication" Language="C#" %>
---
+++ Burberry/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Burberry.Startup))]
+namespace Burberry
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
